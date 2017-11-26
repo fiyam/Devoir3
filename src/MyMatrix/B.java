d31b9c57253e8e358a3c70c6c65e5bae455f26b4
ad091192 <ad091192@C32677.ens.uqam.ca>
2017-11-25 22:17:08 -0300

    question b

--- src/MyMatrix/B.java
+++ src/MyMatrix/B.java
@@ -13,4 +13,28 @@ package MyMatrix;
  */
 public class B {
     
+    public static double trouverParenthesageOptimalRec(int p[],int frontiere[][], int i, int j){
+        double minimum=0;
+        
+        if (i!=j){
+            minimum = Double.POSITIVE_INFINITY;
+            double gauche, droite,total;
+            int frontiereTemp=0;
+            for(int k=i; k<=j-1;k++){
+                // (Ai...Ak)(Ak...Aj)
+                gauche= trouverParenthesageOptimalRec(p,frontiere,i,k);
+                droite = trouverParenthesageOptimalRec(p,frontiere,k+1,j);
+                total= gauche+droite+ (p[i-1] * p[k] * p[j]);
+                if(total<minimum){
+                    minimum=total;
+                    frontiereTemp=k;
+                }
+               
+            }
+            frontiere[i][j]= frontiereTemp;
+            
+        }
+     
+        return minimum;
+    }
 }
