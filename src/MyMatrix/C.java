d434d069d86f95d5e1f206497042129bab91db3d
lmameri <lycia.mameri@gmail.com>
2017-11-27 18:59:58 -0500

    C

--- src/MyMatrix/C.java
+++ src/MyMatrix/C.java
@@ -8,38 +8,43 @@ package MyMatrix;
 import java.util.Arrays;
 
 public class C {
-    
-    public static double trouverParenthesageOptimalRecOptimise(int p[],int frontiere[][], int i, int j){
-        double minimum=0;
-        
-        if (i!=j){
-            minimum = Double.POSITIVE_INFINITY;
-            double gauche=0, droite=0,total=0 ;
-            int frontiereTemp=0;
-            for(int k=i; k<=j-1;k++){
+     public static int trouverParenthesageOptimalRecOptimise(int p[], int frontiere[][], int i, int j) {
+        int minimum = 0;
+
+        if (i != j) {
+            minimum = Integer.MAX_VALUE;
+            int gauche = 0, droite = 0, total = 0;
+            int frontiereTemp = 0;
+            for (int k = i; k <= j - 1; k++) {
                 // (Ai...Ak)(Ak...Aj)
-                
-                double matriceOptimal[][]=null;
-                Arrays.fill(matriceOptimal,-1);
-                
-                if(matriceOptimal[i][k]==-1)
-                    matriceOptimal[i][k]=trouverParenthesageOptimalRecOptimise(p,frontiere,i,k);
 
-                
-                if(matriceOptimal[k+1][j]==-1)
-                    matriceOptimal[k+1][j]=trouverParenthesageOptimalRecOptimise(p,frontiere,k+1,j);
-
-                total= matriceOptimal[i][k] + matriceOptimal[k+1][j] + (p[i-1] * p[k] * p[j]);
-                if(total<minimum){
-                    minimum=total;
-                    frontiereTemp=k;
+                int matriceOptimal[][] = new int[p.length][p.length];
+                for (int l = 0; l < matriceOptimal.length; l++) {
+                    for (int m = 0; m < matriceOptimal[l].length; m++) {
+                        matriceOptimal[l][m] = -1;
+                    }
                 }
+
+                if (matriceOptimal[i][k] == -1) {
+                    matriceOptimal[i][k] = trouverParenthesageOptimalRecOptimise(p, frontiere, i, k);
+                }
+
                
+                if (matriceOptimal[k + 1][j] == -1) {
+                    matriceOptimal[k + 1][j] = trouverParenthesageOptimalRecOptimise(p, frontiere, k + 1, j);
+                }
+
+                total = matriceOptimal[i][k] + matriceOptimal[k + 1][j] + (p[i - 1] * p[k] * p[j]);
+                if (total < minimum) {
+                    minimum = total;
+                    frontiereTemp = k;
+                }
+
             }
-            frontiere[i][j]= frontiereTemp;
-            
+            frontiere[i - 1][j - 1] = frontiereTemp;
+
         }
-     
+
         return minimum;
     }
 }
