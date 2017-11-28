dead65fe2bf9a96464ce6861b50d18f8b78daca9
lmameri <lycia.mameri@gmail.com>
2017-11-27 20:45:34 -0500

    fix D

--- src/MyMatrix/D.java
+++ src/MyMatrix/D.java
@@ -1,30 +1,26 @@
-
 package MyMatrix;
 
-
 public class D {
-    
-    public static void trouverParenthésageOptimal(int n,int p[],double m[][],int frontiere[][])  {
-     
-        double  minimum;
-        int j,frontiereTemp=0;
-        for(int i=1 ; i<=n ; i++)
-           m[i][i]=0;
- 
-        for ( int s=1 ;s<=n-1;s++){
-            for ( int i=1 ;i<=n-s;i++){
-                j=i+s;
-                minimum = Double.POSITIVE_INFINITY;
-                
-                for ( int k=i ;k<=j-1;k++){
-                    double temp = m[i][k] + m[k+1][j] + p[i-1] * p[k] * p[j];
-                    if(temp<minimum){
-                        minimum = temp ;
-                         frontiereTemp=k;
-                        }
+
+    public static void trouverParenthesageOptimal(int n, int p[], int m[][], int frontiere[][]) {
+
+        int minimum;
+        int j, frontiereTemp = 0;
+
+        for (int s = 1; s <= n - 1; s++) {
+            for (int i = 1; i <= n - s; i++) {
+                j = i + s;
+                minimum = Integer.MAX_VALUE;
+                for (int k = i; k <= j - 1; k++) {
+                    int temp = m[i - 1][k - 1] + m[k][j - 1] + (p[i - 1] * p[k] * p[j]);
+
+                    if (temp < minimum) {
+                        minimum = temp;
+                        frontiereTemp = k;
                     }
-                m[i][j]=minimum;
-                frontiere[i][j]=frontiereTemp;
+                }
+                m[i - 1][j - 1] = minimum;
+                frontiere[i - 1][j - 1] = frontiereTemp;
             }
         }
     }
